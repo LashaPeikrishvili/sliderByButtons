eca88dee4bd05d9a1b77cb2bb5f90f59d08a506c
LashaPeikrishvili <lasha.peikrishvili17b@geolab.edu.ge>
2022-08-01 13:16:32 +0400

    is done

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
+let next = document.querySelector(".nxt")
+let previus = document.querySelector(".pre")
 let images = ["1.jpg", "2.jpg", "3.jpg"]
 let count = 0;
 
 let div = document.querySelector(".mainDiv")
 div.style.backgroundImage = `url(${images[count]})`
+
+next.addEventListener("click", function(){
+    console.log(count);
+    count++
+   if(count > images.length-1){
+    count = 0
+   }
+    div.style.backgroundImage = `url(${images[count]})`
+})
+
+previus.addEventListener("click", function(){
+    //div.style.backgroundImage = `url(${images[count-=1]})`
+    count--
+    console.log(count);
+    if(count < 0){
+        count = images.length-1
+    }
+    div.style.backgroundImage = `url(${images[count]})`
+})
